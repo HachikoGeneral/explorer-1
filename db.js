05453a05c85f3dad349127e396fbf28d5130c79e
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-04-06 11:35:56 +0100

    Update db.js

--- db.js
+++ db.js
@@ -79,14 +79,16 @@ var pack = new ConventionPack();
         ConventionRegistry.Register("My Solution Conventions", pack, t => true);
 
 // create indices
-Transaction.index({timestamp:-1});
-Transaction.index({blockNumber:-1});
-Transaction.index({from:1, blockNumber:-1});
-Transaction.index({to:1, blockNumber:-1});
-Account.index({balance:-1});
-Account.index({balance:-1, blockNumber:-1});
-Block.index({miner:1});
-Block.index({miner:1, blockNumber:-1});
+Transaction.index({ blockNumber: -1 });
+Transaction.index({ from: 1, blockNumber: -1 });
+Transaction.index({ to: 1, blockNumber: -1 });
+Transaction.index({ creates: 1, blockNumber: -1 });
+Account.index({ balance: -1 });
+Account.index({ balance: -1, blockNumber: -1 });
+Account.index({ type: -1, balance: -1 });
+Block.index({ miner: 1 });
+Block.index({ miner: 1, blockNumber: -1 });
+Block.index({ hash: 1, number: -1 });
 
 mongoose.model('BlockStat', BlockStat);
 mongoose.model('Block', Block);
